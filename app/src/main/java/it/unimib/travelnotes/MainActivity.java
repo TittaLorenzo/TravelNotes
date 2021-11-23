65b9262e143ddaf0d66475d341f15136a16af6cd
LorenzoTitta <l.titta1@campus.unimib.it>
2021-11-23 16:36:03 +0100

    Creazione classe listaViaggi
    creazione della lista viaggi
    passaggio da activity login a lista viaggi

--- app/src/main/java/it/unimib/travelnotes/MainActivity.java
+++ app/src/main/java/it/unimib/travelnotes/MainActivity.java
@@ -2,7 +2,9 @@ package it.unimib.travelnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //bottone login che porta alla lista viaggi
+        final Button mButtonNext = findViewById(R.id.L_button);
+        mButtonNext.setOnClickListener(v -> {
+            Intent intent = new Intent(this, TravelList.class);
+            startActivity(intent);
+        });
     }
 }
---
+++ app/src/main/java/it/unimib/travelnotes/TravelList.java
@@ -0,0 +1,14 @@
+package it.unimib.travelnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TravelList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travel_list);
+    }
+}
