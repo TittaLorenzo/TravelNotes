4438322c46abc979157d6a0c51274c47f34988f0
Edoardo <edocolombo@hotmail.it>
2021-11-25 01:14:28 +0100

    Inizio grafica TravelsList

--- app/src/main/java/it/unimib/travelnotes/TravelList.java
+++ app/src/main/java/it/unimib/travelnotes/TravelList.java
@@ -2,13 +2,23 @@ package it.unimib.travelnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class TravelList extends AppCompatActivity {
+
+    private Button mButtonNew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_travel_list);
+
+        mButtonNew = findViewById(R.id.new_travel);
+
+        mButtonNew.setOnClickListener(v -> {
+            Intent intent = new Intent(this, NewTravel.class);
+        });
     }
 }
