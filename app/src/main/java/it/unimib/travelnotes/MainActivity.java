68e0e12d260c2ade2352835724966c0a17e59eae
Simone Confalonieri <s.confalonieri@campus.unimib.it>
2021-11-28 12:52:50 +0100

    primo sketch NewAactivity

--- app/src/main/java/it/unimib/travelnotes/MainActivity.java
+++ app/src/main/java/it/unimib/travelnotes/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         //bottone login che porta alla lista viaggi
         final Button mButtonNext = findViewById(R.id.L_button);
         mButtonNext.setOnClickListener(v -> {
-            Intent intent = new Intent(this, TravelList.class);
+            Intent intent = new Intent(this, NewActivityEvent.class);
             startActivity(intent);
         });
     }
