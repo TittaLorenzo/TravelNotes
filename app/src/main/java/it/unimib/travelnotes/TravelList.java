9624d2f50c9cae2d4227aca534b5212d1f8eae07
Edoardo <edocolombo@hotmail.it>
2021-11-29 08:56:50 +0100

    Recyclerview per card viaggi
    Creazione di classe TravelAdapter, aggiunta icone card viaggio.

--- app/src/main/java/it/unimib/travelnotes/TravelList.java
+++ app/src/main/java/it/unimib/travelnotes/TravelList.java
@@ -1,24 +1,79 @@
 package it.unimib.travelnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class TravelList extends AppCompatActivity {
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.unimib.travelnotes.Model.Viaggio;
+import it.unimib.travelnotes.databinding.ActivityTravelListBinding;
+
+public class TravelList extends AppCompatActivity implements View.OnClickListener {
 
     private Button mButtonNew;
+    private ActivityTravelListBinding binding;
+    private List<Viaggio> travelList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_travel_list);
 
-        mButtonNew = findViewById(R.id.new_travel);
 
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        Viaggio[] viaggio = new Viaggio[]{
+                new Viaggio("Milano", "Parigi", "Parigi", "Milano", 90, 90),
+                new Viaggio("Roma", "Berlino", "Berlino", "Roma", 90, 90),
+                new Viaggio("Napoli", "Palermo", "Palermo", "Napoli", 90, 90),
+                new Viaggio("Parigi", "Tokyo", "Tokyo", "Parigi", 90, 90),
+                new Viaggio("Los Angeles", "New York", "Los Angeles", "New York", 90, 90),
+                new Viaggio("Berlino", "Tokyo", "Tokyo", "Berlino", 90, 90),
+                new Viaggio("Madrid", "Porto", "Porto", "Madrid", 90, 90),
+                new Viaggio("Lisbona", "Barcellona", "Barcellona", "Lisbona", 90, 90),
+                new Viaggio("Milano", "Roma", "Roma", "Milano", 90, 90),
+                new Viaggio("Los Angeles", "New York", "Los Angeles", "New York", 90, 90),
+                new Viaggio("Milano", "Parigi", "Parigi", "Milano", 90, 90),
+                new Viaggio("Roma", "Berlino", "Berlino", "Roma", 90, 90),
+                new Viaggio("Napoli", "Palermo", "Palermo", "Napoli", 90, 90),
+                new Viaggio("Parigi", "Tokyo", "Tokyo", "Parigi", 90, 90),
+                new Viaggio("Los Angeles", "New York", "Los Angeles", "New York", 90, 90),
+                new Viaggio("Berlino", "Tokyo", "Tokyo", "Berlino", 90, 90),
+                new Viaggio("Madrid", "Porto", "Porto", "Madrid", 90, 90),
+                new Viaggio("Lisbona", "Barcellona", "Barcellona", "Lisbona", 90, 90),
+                new Viaggio("Milano", "Roma", "Roma", "Milano", 90, 90),
+                new Viaggio("Los Angeles", "New York", "Los Angeles", "New York", 90, 90),
+        };
+
+        TravelAdapter travelAdapter = new TravelAdapter(viaggio, TravelList.this);
+        recyclerView.setAdapter(travelAdapter);
+
+
+
+        mButtonNew = findViewById(R.id.new_travel);
         mButtonNew.setOnClickListener(v -> {
             Intent intent = new Intent(this, NewTravel.class);
         });
+/*
+        binding = ActivityTravelListBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        travelList.add(new Viaggio("a","b","c","d",1,2));
+*/
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
