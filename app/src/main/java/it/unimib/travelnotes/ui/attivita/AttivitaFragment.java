3db60729728ca341dbc719c742edd04a34a83484
DavideSoldati <d.soldati2@campus.unimib.it>
2021-12-27 17:03:11 +0100

    button nuova attivita

--- app/src/main/java/it/unimib/travelnotes/ui/attivita/AttivitaFragment.java
+++ app/src/main/java/it/unimib/travelnotes/ui/attivita/AttivitaFragment.java
@@ -1,35 +1,57 @@
 package it.unimib.travelnotes.ui.attivita;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
+import it.unimib.travelnotes.Activity_travel_view;
+import it.unimib.travelnotes.Model.Attivita;
+import it.unimib.travelnotes.NewActivityEvent;
+import it.unimib.travelnotes.R;
 import it.unimib.travelnotes.databinding.FragmentAttivitaBinding;
+import it.unimib.travelnotes.databinding.FragmentFlightBinding;
 
 public class AttivitaFragment extends Fragment {
 
-    //private DashboardViewModel dashboardViewModel;
+    private Attivita[] mAttivitaArray;
+    private List<Attivita> mAttivitaList;
     private FragmentAttivitaBinding binding;
+   
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-       // dashboardViewModel =
-        //        new ViewModelProvider(this).get(DashboardViewModel.class);
-
         binding = FragmentAttivitaBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        /*final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        Button button_n_attivita = (Button) root.findViewById(R.id.new_attivita);
+        button_n_attivita.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), NewActivityEvent.class);
+                startActivity(intent);
             }
+        });
+
+        /*final Button attivitaUno = getView().findViewById(R.id.button_nuova_attivita);
+        attivitaUno.setOnClickListener(v -> {
+            Intent i = new Intent(getActivity(), NewActivityEvent.class);
+            i.putExtra("idAttivita", 1);
+            startActivity(i);
         });*/
+
+
+
+
         return root;
     }
 
