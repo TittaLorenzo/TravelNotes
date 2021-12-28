25f237ad674308affcb84f800ff9a7a711005ee0
DavideSoldati <d.soldati2@campus.unimib.it>
2021-12-28 17:48:16 +0100

    scroll view fragment volo

--- app/src/main/java/it/unimib/travelnotes/ui/flight/FlightFragment.java
+++ app/src/main/java/it/unimib/travelnotes/ui/flight/FlightFragment.java
@@ -12,26 +12,21 @@ import it.unimib.travelnotes.databinding.FragmentFlightBinding;
 
 public class FlightFragment extends Fragment {
 
-    // private HomeViewModel homeViewModel;
+
     private FragmentFlightBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        // homeViewModel =
-                // new ViewModelProvider(this).get(HomeViewModel.class);
+
 
         binding = FragmentFlightBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        // final TextView textView = binding.textHome;
-        /*homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });*/
+
+
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
