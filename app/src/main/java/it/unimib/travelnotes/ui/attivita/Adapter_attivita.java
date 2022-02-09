e9c018d3301f1ab217b416c9fe047e8b991e7ca8
DavideSoldati <d.soldati2@campus.unimib.it>
2022-02-09 14:27:07 +0100

    button modifica attivita

--- app/src/main/java/it/unimib/travelnotes/ui/attivita/Adapter_attivita.java
+++ app/src/main/java/it/unimib/travelnotes/ui/attivita/Adapter_attivita.java
@@ -1,8 +1,10 @@
 package it.unimib.travelnotes.ui.attivita;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +14,7 @@ import java.util.List;
 
 import it.unimib.travelnotes.Model.Attivita;
 import it.unimib.travelnotes.R;
+import it.unimib.travelnotes.ui.newactivityevent.NewActivityEvent;
 
 public class Adapter_attivita extends RecyclerView.Adapter<Adapter_attivita.AttivitaViewHolder> {
     private List<Attivita> attivitaList;
@@ -68,11 +71,24 @@ public class Adapter_attivita extends RecyclerView.Adapter<Adapter_attivita.Atti
     public class AttivitaViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         TextView attivitaNome;
         TextView descrizione;
+        ImageButton modificaAttivita;
 
         public AttivitaViewHolder(@NonNull View itemView) {
             super(itemView);
             attivitaNome = itemView.findViewById(R.id.nome_attivita);
             descrizione = itemView.findViewById(R.id.descrizione);
+            modificaAttivita =  itemView.findViewById(R.id.button_modifica_attivita);
+            modificaAttivita.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = (new Intent(view.getContext(), NewActivityEvent.class));
+                    intent.putExtra("modifica_attivita", true);
+                    view.getContext().startActivity(intent);
+
+
+
+                }
+            });
         }
 
 
--- app/src/main/java/it/unimib/travelnotes/ui/attivita/AttivitaFragment.java
+++ app/src/main/java/it/unimib/travelnotes/ui/attivita/AttivitaFragment.java
@@ -98,6 +98,7 @@ public class AttivitaFragment extends Fragment  {
                 startActivity(intent);
             }
         });
+
         return view;
     }
 
